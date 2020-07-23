7c230c90a5a0fc382b9285683279faeb9aabccc2
Austin Way <AustinWay@bge.local>
2020-07-23 08:53:17 -0500

    Initial Commit

---
+++ ASP NET CORE MVC Authentication/Controllers/RolesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Reach_Core.Models;
+
+
+namespace Reach_Core.Controllers
+{
+    public class RolesController : Controller
+    {
+        private RoleManager<IdentityRole> roleManager;
+        private UserManager<IdentityUser> userManager;
+        public RolesController(RoleManager<IdentityRole> roleMgr, UserManager<IdentityUser> userMgr)
+        {
+            roleManager = roleMgr;
+            userManager = userMgr;
+        }
+
+        
+        public ViewResult Index() => View(roleManager.Roles);
+
+        public ViewResult Create()
+        {
+            return View();
+
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Required] string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    Errors(result);
+            }
+            return View(name);
+        }
+
+        public async Task<IActionResult> ManageUsers(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            List<IdentityUser> members = new List<IdentityUser>();
+            List<IdentityUser> nonMembers = new List<IdentityUser>();
+            foreach (IdentityUser user in userManager.Users)
+            {
+                var list = await userManager.IsInRoleAsync(user, role.Name) ? members : nonMembers;
+                list.Add(user);
+            }
+            return View(new RoleEdit
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            });
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ManageUsers(RoleModification model)
+        {
+            IdentityResult result;
+            if (ModelState.IsValid)
+            {
+                foreach (string userId in model.AddIds ?? new string[] { })
+                {
+                    IdentityUser user = await userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        result = await userManager.AddToRoleAsync(user, model.RoleName);
+                        if (!result.Succeeded)
+                            Errors(result);
+                    }
+                }
+                foreach (string userId in model.DeleteIds ?? new string[] { })
+                {
+                    IdentityUser user = await userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        result = await userManager.RemoveFromRoleAsync(user, model.RoleName);
+                        if (!result.Succeeded)
+                            Errors(result);
+                    }
+                }
+            }
+
+            if (ModelState.IsValid)
+                return RedirectToAction(nameof(Index));
+            else
+                return await ManageUsers(model.RoleId);
+        }
+
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
+        }
+    }
+}
